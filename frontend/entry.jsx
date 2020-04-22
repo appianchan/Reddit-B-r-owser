12de8f164fe500cec978d6a50a26094202838187
Anthony Chan <appianchan@gmail.com>
2020-04-21 20:16:07 -0700

    added search route in entry file, cleaned up files

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -6,33 +6,31 @@ import Index from './components/index_folder/index';
 import Subreddit from './components/subreddit_folder/subreddit';
 import Searchbar from "./components/searchbar/searchbar";
 import Error from "./components/error/error";
-
+import Search from "./components/Search/search";
 
 
 
 const Root = () => (
   <HashRouter>
     <div className="parent-container">
-      
-        <Link className="Title" to={"/"}>
-        
+      <Link className="Title" to={"/"}>
         <img
           className="logo"
           src={"https://static.tvtropes.org/pmwiki/pub/images/bowser2.png"}
         />
-        <div className="appname">Reddit B(r)owser</div>
-        
+        Reddit B(r)owser
       </Link>
       <Searchbar />
       <Switch>
         <Route exact path="/" component={Index} />
-        <Route exact path="/subreddit/r/:subreddit" component={Subreddit} />
+        <Route path="/search/:topic" component={Search} />
+        <Route exact path="/subreddit//r/:subreddit" component={Subreddit} />
         <Route exact path="/error" component={Error} />
       </Switch>
     </div>
   </HashRouter>
 );
-  
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
